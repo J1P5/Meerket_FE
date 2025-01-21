7b199bcbcb700d2cffe90175d2a7df2aca004636
JW-Ahn0 <jun.woo.an0@gmail.com>
2025-01-22 00:40:57 +0900

    refactor: ImageSlider 컴포넌트 Swiper 지연 로딩하도록 변겨

--- src/components/molecules/ImageSlider/index.tsx
+++ src/components/molecules/ImageSlider/index.tsx
@@ -1,8 +1,18 @@
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Pagination } from "swiper/modules";
+import React, { Suspense } from "react";
 import { Image } from "components/atoms";
+import { Loading } from "../Loading";
 import { ImageSliderWrapper } from "./styled";
+import { Pagination } from "swiper/modules";
 import "swiper/swiper-bundle.css";
+// Swiper 컴포넌트를 지연 로딩
+// eslint-disable-next-line @rushstack/typedef-var
+const Swiper = React.lazy(() =>
+  import("swiper/react").then((module) => ({ default: module.Swiper }))
+);
+// eslint-disable-next-line @rushstack/typedef-var
+const SwiperSlide = React.lazy(() =>
+  import("swiper/react").then((module) => ({ default: module.SwiperSlide }))
+);
 
 // TODO: import 오류 해결하기
 // import "swiper/css";
@@ -17,20 +27,21 @@ interface IImageSliderProps {
 export const ImageSlider = ({ imgUrls }: IImageSliderProps) => {
   return (
     <ImageSliderWrapper>
-      <Swiper
-        pagination={{
-          type: "fraction"
-        }}
-        modules={[Pagination]}
-        className="image-slider"
-      >
-        {imgUrls.map((url, index) => (
-          <SwiperSlide key={index}>
-            <Image url={url} />
-          </SwiperSlide>
-        ))}
-      </Swiper>
+      <Suspense fallback={<Loading />}>
+        <Swiper
+          pagination={{
+            type: "fraction",
+          }}
+          modules={[Pagination]}
+          className="image-slider"
+        >
+          {imgUrls.map((url, index) => (
+            <SwiperSlide key={index}>
+              <Image url={url} />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </Suspense>
     </ImageSliderWrapper>
   );
 };
-
