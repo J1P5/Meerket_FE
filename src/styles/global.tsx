b696f765ff1895f9cb962e435e8f2344049c6dff
y0unj1NoH <nohyoong711@gmail.com>
2025-01-24 02:16:14 +0900

    fix: woff2 로드 에러 수정

--- src/styles/global.tsx
+++ src/styles/global.tsx
@@ -8,8 +8,10 @@ export const GlobalStyle = () => {
 
         @font-face {
           font-family: "Pretendard";
-          src: url("https://fastly.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff2")
+          src: url("https://fastly.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff")
             format("woff2");
+          src: url("https://fastly.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff")
+            format("woff");
           font-weight: 400;
           font-style: normal;
           font-display: swap;
@@ -17,8 +19,10 @@ export const GlobalStyle = () => {
 
         @font-face {
           font-family: "Pretendard";
-          src: url("https://fastly.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-SemiBold.woff2")
+          src: url("https://fastly.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-SemiBold.woff")
             format("woff2");
+          src: url("https://fastly.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-SemiBold.woff")
+            format("woff");
           font-weight: 600;
           font-style: normal;
           font-display: swap;
@@ -26,8 +30,10 @@ export const GlobalStyle = () => {
 
         @font-face {
           font-family: "Pretendard";
-          src: url("https://fastly.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Bold.woff2")
+          src: url("https://fastly.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Bold.woff")
             format("woff2");
+          src: url("https://fastly.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Bold.woff")
+            format("woff");
           font-weight: 700;
           font-style: normal;
           font-display: swap;
