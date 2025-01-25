e2aabc32cbc480ebcc46649bd63fb3a65c1329e6
y0unj1NoH <nohyoong711@gmail.com>
2025-01-25 17:16:32 +0900

    refactor: Text 컴포넌트 children으로 수정

--- src/components/atoms/Text/index.tsx
+++ src/components/atoms/Text/index.tsx
@@ -33,8 +33,6 @@ export type TextVariant =
   | "badge_regular";
 
 interface ITextProps extends HTMLAttributes<HTMLParagraphElement> {
-  /** Text에 들어가는 내용 */
-  content: string;
   /** Text의 타입 (스타일 가이드 준수) */
   variant?: TextVariant;
   /** Text의 색깔 */
@@ -63,7 +61,7 @@ const variantMap: {
 };
 
 export const Text = ({
-  content,
+  children,
   variant = "body1",
   onClick = () => {},
   color,
@@ -71,7 +69,7 @@ export const Text = ({
   const Component = variantMap[variant];
   return (
     <Component onClick={onClick} color={color}>
-      {content}
+      {children}
     </Component>
   );
 };
