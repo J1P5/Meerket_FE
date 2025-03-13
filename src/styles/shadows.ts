690e30a499a694ed914b754ca89660f94ab5e001
y0unj1NoH <nohyoong711@gmail.com>
2025-03-13 23:50:11 +0900

    feat: shadows 추가

---
+++ src/styles/shadows.ts
@@ -0,0 +1,8 @@
+
+type ShadowSizeKey = "sm" | "md" | "lg";
+
+export const shadows: Record<ShadowSizeKey, string> = { 
+  sm: '0 0 5px rgba(19, 27, 83, 0.05)',
+  md: '0 0 10px rgba(19, 27, 83, 0.05)',
+  lg: '0 0 20px rgba(19, 27, 83, 0.05)',
+};
