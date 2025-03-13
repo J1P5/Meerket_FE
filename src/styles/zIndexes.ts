e1df0d0983329de85f32f710ee0c3dd8175b08c1
y0unj1NoH <nohyoong711@gmail.com>
2025-03-14 00:43:05 +0900

    chore: prettier 적용

--- src/styles/zIndexes.ts
+++ src/styles/zIndexes.ts
@@ -1,8 +1,15 @@
-type ZIndexKey = 'RootLayout' | 'KebabMenu' | 'TopSheet' | 'BottomSheet' | 'ControlBar' | 'Modal' | 'Toast';
+type ZIndexKey =
+  | 'RootLayout'
+  | 'KebabMenu'
+  | 'TopSheet'
+  | 'BottomSheet'
+  | 'ControlBar'
+  | 'Modal'
+  | 'Toast';
 
 export const zIndexes: Record<ZIndexKey, number> = {
-  RootLayout: 100,  
-  KebabMenu: 200,  
+  RootLayout: 100,
+  KebabMenu: 200,
   TopSheet: 300,
   BottomSheet: 300,
   ControlBar: 400,
