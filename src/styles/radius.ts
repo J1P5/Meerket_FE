d58eaad64a5a0f8950fee5a582afdff8d87ae86a
y0unj1NoH <nohyoong711@gmail.com>
2025-03-13 23:50:01 +0900

    feat: radius 추가

---
+++ src/styles/radius.ts
@@ -0,0 +1,10 @@
+type RadiusSizeKey = "sm" | "md" | "lg" | "xl" | "round";
+
+export const radius: Record<RadiusSizeKey, string> = {
+    sm: '4px',
+    md: '8px',
+    lg: '10px',
+    xl: '16px',
+    round: '50%',
+};
+  
