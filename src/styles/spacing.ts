faeecbe2ff7d440f8ad0477d512fdd953f4083ae
y0unj1NoH <nohyoong711@gmail.com>
2025-03-14 00:05:35 +0900

    feat: spacing 추가

---
+++ src/styles/spacing.ts
@@ -0,0 +1,11 @@
+type SpacingSizeKey = "xxs" | "xs" | "sm" | "md" | "lg" | "xl" | "xxl";
+
+export const spacing: Record<SpacingSizeKey, string> = {
+    xxs: '0.25rem', // 4px
+    xs: '0.5rem',   // 8px
+    sm: '0.75rem',   // 12px
+    md: '1rem',      // 16px
+    lg: '1.5rem',    // 24px
+    xl: '2rem',     // 32px
+    xxl: '3rem',    // 48px
+}
