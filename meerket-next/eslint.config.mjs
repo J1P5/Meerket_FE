65fa74795e3ec446d3e39af2344f40c327d575eb
JW-Ahn0 <jun.woo.an0@gmail.com>
2025-03-27 12:06:29 +0900

    init: Prettier eslint 설정 추가

--- meerket-next/eslint.config.mjs
+++ meerket-next/eslint.config.mjs
@@ -1,16 +1,20 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+import { FlatCompat } from '@eslint/eslintrc'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-});
+  recommendedConfig: {}, // 이 부분 추가
+})
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+  ...compat.extends('next/core-web-vitals'),
+  ...compat.extends('@titicaca/eslint-config-triple'),
+  ...compat.extends('@titicaca/eslint-config-triple/requiring-type-checking'),
+  ...compat.extends('@titicaca/eslint-config-triple/prettier'),
+]
 
-export default eslintConfig;
+export default eslintConfig
