8f8c94ec3150631407e8db52244933e0fd0c7ae5
y0unj1NoH <nohyoong711@gmail.com>
2025-03-28 11:22:31 +0900

    chore: 빌드 에러 발생

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
-import { Suspense } from "react";
-import { RouterProvider } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { GlobalStyle } from "styles";
-import { useForegroundNotification } from "hooks";
-import { Modal } from "components/organisms";
-import { useModalStore } from "stores";
-import { Loading } from "components/molecules/Loading";
-import { router } from "router";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { Loading } from 'components/molecules/Loading';
+import { Modal } from 'components/organisms';
+import { useForegroundNotification } from 'hooks';
+import { Suspense } from 'react';
+import { RouterProvider } from 'react-router-dom';
+import { router } from 'router';
+import { useModalStore } from 'stores';
+import { GlobalStyle } from 'styles';
 const queryClient: QueryClient = new QueryClient();
 
 const App = () => {
   useForegroundNotification();
-  const isOpen = useModalStore((store) => store.isOpen);
+  const isopen = useModalStore((store) => store.isOpen);
   const content = useModalStore((store) => store.content);
   const { closeModal } = useModalStore((store) => store.actions);
 
