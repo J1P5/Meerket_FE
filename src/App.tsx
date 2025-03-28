d444fa5fd5300bb1977daaeac34f33bcd20c7b77
y0unj1NoH <nohyoong711@gmail.com>
2025-03-28 14:13:55 +0900

    chore: 빌드 에러 수정

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ const queryClient: QueryClient = new QueryClient();
 
 const App = () => {
   useForegroundNotification();
-  const isopenError = useModalStore((store) => store.isOpen);
+  const isOpen = useModalStore((store) => store.isOpen);
   const content = useModalStore((store) => store.content);
   const { closeModal } = useModalStore((store) => store.actions);
 
