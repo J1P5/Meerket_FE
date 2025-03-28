fe6aeff20fabec85528f409d754e2f5832d6b51f
y0unj1NoH <nohyoong711@gmail.com>
2025-03-28 15:49:50 +0900

    fix: 다중 이미지 선택 가능하도록 수정

--- src/components/atoms/ImageUpload/index.tsx
+++ src/components/atoms/ImageUpload/index.tsx
@@ -1,20 +1,30 @@
-import { ImageUploadWrapper } from "./styled";
+import { ImageUploadWrapper } from './styled';
 
 interface IImageUploadProps {
   /** 파일을 선택했을 때 동작하는 로직,이후 파라미터 같은 부분 별도 커스텀 필요 합니다. */
-  onFileChange: (file: File) => void;
+  onFileChange: (file: FileList) => void;
+  /** 다중 파일 업로드 가능 여부 */
+  multiple?: boolean;
 }
-export const ImageUpload = ({ onFileChange }: IImageUploadProps) => {
+export const ImageUpload = ({
+  onFileChange,
+  multiple = false,
+}: IImageUploadProps) => {
   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const file = event.target.files?.[0];
-    if (file) {
+    const file = event.target.files;
+    if (file?.length) {
       onFileChange(file);
     }
   };
 
   return (
     <ImageUploadWrapper>
-      <input type="file" onChange={handleFileChange} aria-label="imageUploader" />
+      <input
+        type="file"
+        multiple={multiple}
+        onChange={handleFileChange}
+        aria-label="imageUploader"
+      />
     </ImageUploadWrapper>
   );
 };
--- src/components/atoms/ImageUpload/stories.ts
+++ src/components/atoms/ImageUpload/stories.ts
@@ -1,19 +1,30 @@
-import type { Meta, StoryObj } from "@storybook/react";
-import { ImageUpload } from ".";
+import type { Meta, StoryObj } from '@storybook/react';
+import { ImageUpload } from '.';
 
 const meta: Meta<typeof ImageUpload> = {
-  title: "Atoms/ImageUpload",
+  title: 'Atoms/ImageUpload',
   component: ImageUpload,
-  tags: ["autodocs"],
+  tags: ['autodocs'],
 };
 export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const defaultState: Story = {
   args: {
-    onFileChange: (file) => {
+    onFileChange: (fileList) => {
       // 파일명을 콘솔에 출력
-      console.log("Selected file:", file.name);
+      console.log('Selected file:', fileList[0].name);
     },
+  },
+};
+
+export const Multiple: Story = {
+  args: {
+    onFileChange: (fileList) => {
+      // 파일명을 콘솔에 출력
+      const fileNames = Array.from(fileList).map((file: File) => file.name);
+      console.log('Selected file:', fileNames);
+    },
+    multiple: true,
   },
 };
