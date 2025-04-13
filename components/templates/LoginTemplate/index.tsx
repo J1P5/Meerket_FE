4bbc95da8e2e33f320f0f38c8984547b12eab851
y0unj1NoH <nohyoong711@gmail.com>
2025-04-14 07:18:25 +0900

    fix: copy로 변경

--- components/templates/LoginTemplate/index.tsx
+++ components/templates/LoginTemplate/index.tsx
@@ -1,7 +1,5 @@
-// 'use client'
-
 import { LoginButton } from '@/components/atoms'
-import { WELL_COME_PAGE_TEXT } from '@/constants/Login'
+import { WELCOME_PAGE_TEXT } from '@/constants'
 import Logo from '@/public/Logo.svg'
 
 interface IComponentProps {
@@ -18,7 +16,7 @@ export const LoginTemplate = ({
   return (
     <div className="flex flex-col justify-between flex-1 pt-8 px-4 pb-4">
       <p className="text-[var(--color-black)] text-[1rem] font-bold leading-[145%]">
-        {WELL_COME_PAGE_TEXT}
+        {WELCOME_PAGE_TEXT}
       </p>
       <Logo className="self-center w-32 md:w-40" />
       <div className="flex flex-col gap-2 mt-12 w-full">
---
+++ constants/copy.ts
@@ -0,0 +1,5 @@
+/**
+ * 로그인 페이지
+ */
+export const WELCOME_PAGE_TEXT: string =
+  '사는 재미와 파는 재미,\n미어켓과 중고 거래를 시작해보세요!'
