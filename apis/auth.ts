c3422565e76a22a2cb0c993ea20b080f6a8cb4ba
y0unj1NoH <nohyoong711@gmail.com>
2025-04-14 04:20:50 +0900

    feat: apis 관련 코드 추가 (fetch로 수정 필요)

---
+++ apis/auth.ts
@@ -0,0 +1,41 @@
+import { http } from '@/apis'
+import type { IAuthData, IAuthResponse, OAuthProvider } from '@/types'
+
+const redirectUrl: string = `${process.env.NEXT_PUBLIC_CLIENT_URL}/login/callback`
+const authorizeUrl: Record<OAuthProvider, string> = {
+  KAKAO: `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${
+    process.env.NEXT_PUBLIC_OAUTH_KAKAO_KEY
+  }&redirect_uri=${redirectUrl}/kakao`,
+  NAVER: `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${
+    process.env.NEXT_PUBLIC_OAUTH_NAVER_KEY
+  }&redirect_uri=${redirectUrl}/naver`,
+}
+
+/**
+ * provider의 authorize 페이지로 이동하는 함수
+ * @param provider
+ */
+export const requestOAuthLogin = (provider: OAuthProvider) => {
+  window.open(authorizeUrl[provider], '_blank')
+}
+
+/**
+ * oauth 로그인
+ * @param code
+ * @param provider
+ * @param fcmToken
+ */
+export const oauthLogin = async ({ code, provider, fcmToken }: IAuthData) => {
+  return http.post<IAuthResponse, IAuthData>('/oauth', {
+    code,
+    provider,
+    fcmToken,
+  })
+}
+
+/**
+ * 로그아웃
+ */
+export const oauthLogout = async () => {
+  return http.post('/oauth/logout')
+}
