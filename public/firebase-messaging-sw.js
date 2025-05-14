5c88d17e613289eba7ccdc4d6f616e78dcc08f37
y0unj1NoH <nohyoong711@gmail.com>
2025-05-15 07:36:09 +0900

    chore: fcm 백그라운드 notificaton으로 변경 후 테스트 (ver. fcm_options)

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -10,6 +10,10 @@ const firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig);
 
+// 이미 백그라운드에 있으면 보고 잇는 페이지로 포커스만 되고, 페이지가 없으면 클릭하면 거기로 감
+// 내가 원하는 곳으로 링크가 되지 않아서 차라리 커스텀을 하는 게 나을 것 같음
+// 그러면 notification을 쓸 필요가 없고, 그냥 내가 data로 커스텀을 하는게 나을 것 같다
+// 모바일 테스트만 해보고 팝업 형식으로 어차피 뜨지 않는다면 그냥 data로 돌아가는 게 나을 듯
 const messaging = firebase.messaging();
 messaging.onBackgroundMessage((payload) => {
   console.log(
@@ -17,24 +21,34 @@ messaging.onBackgroundMessage((payload) => {
     payload
   );
 
-  const {title, content, uri} =  payload.data;
-  const options = {
-    body: content,
-    icon: "/icons/logo-128x128.png",
-    data: {
-      link: uri || "/",
-    },
-    badge: "/icons/logo-128x128.png",
-  };
+  // const {title, body} =  payload.notification;
+  // const options = {
+  //   body,
+  //   icon: "/icons/logo-128x128.png",
+  //   badge: "/icons/logo-128x128.png",
+  // };
 
-  self.registration.showNotification(title, options);
+  // self.registration.showNotification(title, options);
+
+  // const {title, content, uri} =  payload.data;
+  // const options = {
+  //   body: content,
+  //   icon: "/icons/logo-128x128.png",
+  //   data: {
+  //     link: uri || "/",
+  //   },
+  //   badge: "/icons/logo-128x128.png",
+  // };
+
+  // self.registration.showNotification(title, options);
 });
 
-self.addEventListener("notificationclick", (event) => {
-  const { link } = event.notification.data;
-  event.notification.close();
-  event.waitUntil(clients.openWindow(link || "/"));
-});
+// self.addEventListener("notificationclick", (event) => {
+//   console.log("notificationclick", event);
+//   const { link } = event.notification.data;
+//   event.notification.close();
+//   event.waitUntil(clients.openWindow(link || "/"));
+// });
 
 /**
  * 백그라운드 알림 처리
